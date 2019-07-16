0672cb2aa15cf543afc820f4dba8c63bb28f08b5
Iain Skett <iain.skett@gmail.com>
2019-07-16 23:24:46 +0100

    Switch the package so we can register rdates separately and add appropriate show messages

--- src/RDates.jl
+++ src/RDates.jl
@@ -1,12 +1,17 @@
 module RDates
 
+include("grammar.jl")
+
 include("monthinc.jl")
 include("invalidday.jl")
 
-include("rdate.jl")
+# The various basic implementations (along with shows and grammar registrations)
+include("abstracts.jl")
+include("basics.jl")
+include("compounds.jl")
+
 include("ranges.jl")
-include("grammar.jl")
-include("io.jl")
+# include("io.jl")
 # Export the macro and non-macro parsers.
 export @rd_str
 export rdate
