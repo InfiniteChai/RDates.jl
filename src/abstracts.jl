901eaaf491e2df1cf32ce4d2ce19de7d0804daa1
Iain Skett <iain.skett@gmail.com>
2019-09-25 22:37:08 +0100

    Add support for null calendar manager and basic addition

--- src/abstracts.jl
+++ src/abstracts.jl
@@ -10,10 +10,16 @@ using Compat
 @compat abstract type InvalidDayConvention end
 @compat abstract type MonthIncrementConvention end
 
+struct NullCalendarManager <: CalendarManager end
+
 is_holiday(x::Calendar, ::Dates.Date) = error("$(typeof(x)) does not support is_holiday")
 calendar(x::CalendarManager, ::String)::Calendar = error("$(typeof(x)) does not support calendar")
 
 apply(rd::RDate, ::Dates.Date, ::CalendarManager)::Dates.Date = error("$(typeof(rd)) does not support date apply")
+apply(rd::RDate, date::Dates.Date) = apply(rd, date, NullCalendarManager())
+# TODO: Look into whether we can introduce a calendar manager for this purpose.
+Base.:+(rd::RDate, date::Dates.Date) = apply(rd, date)
+Base.:+(date::Dates.Date, rd::RDate) = apply(rd, date)
 Base.:-(x::RDate)::RDate = error("$(typeof(x)) does not support negation")
 
 apply(x::HolidayRoundingConvention, ::Dates.Date, ::Calendar)::Dates.Date = error("$(typeof(x)) does not support apply")
